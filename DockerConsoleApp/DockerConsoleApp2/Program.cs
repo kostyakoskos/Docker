62fd2d3001179f8736acceff558633512380ab6d
kostyakoskos <kanstantsin.karatkevich@arcadia.spb.ru>
2021-10-05 10:38:37 +0300

    Docker 05.10.2021

---
+++ DockerConsoleApp/DockerConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+// docker build --file ./Dockerfile --no-cache --tag dotnetdocker:1.0 .
+// docker ignore **/*.*proj.user
+namespace DockerConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello console docker"); 
+            Console.WriteLine("rebuild");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ DockerfileBuildFromCMD/DockerfileBuildFromCMD/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DockerfileBuildFromCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello app");
+            Console.WriteLine("Hello Docker!");
+        }
+    }
+}
